ad1d8994ae21e86d45e50c7207a19f7c66d0194a
Ariel <ariel.lissak@gmail.com>
2023-12-02 11:55:32 +0100

    Question 14 TP1

--- app/src/main/java/com/todoarielthibault/todo/list/TaskListAdapter.kt
+++ app/src/main/java/com/todoarielthibault/todo/list/TaskListAdapter.kt
@@ -1,16 +1,14 @@
-package com.todoarielthibault.todo.list
-
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.todoarielthibault.todo.model.Task
 import com.todoarielthibault.todo.R
 
-class TaskListAdapter : RecyclerView.Adapter<TaskListAdapter.TaskViewHolder>() {
-
-    var currentList: List<Task> = emptyList()
+class TaskListAdapter : ListAdapter<Task, TaskListAdapter.TaskViewHolder>(TaskDiffCallback()) {
 
     inner class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val taskTitleTextView: TextView = itemView.findViewById(R.id.taskTitleTextView)
@@ -28,10 +26,16 @@ class TaskListAdapter : RecyclerView.Adapter<TaskListAdapter.TaskViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
-        holder.bind(currentList[position])
+        holder.bind(getItem(position))
     }
 
-    override fun getItemCount(): Int {
-        return currentList.size
+    private class TaskDiffCallback : DiffUtil.ItemCallback<Task>() {
+        override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean {
+            return oldItem == newItem
+        }
     }
 }
--- app/src/main/java/com/todoarielthibault/todo/list/TaskListFragment.kt
+++ app/src/main/java/com/todoarielthibault/todo/list/TaskListFragment.kt
@@ -1,13 +1,16 @@
 package com.todoarielthibault.todo.list
 
+import TaskListAdapter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.todoarielthibault.todo.R
 import com.todoarielthibault.todo.model.Task
+import java.util.*
 
 class TaskListFragment : Fragment() {
 
@@ -23,14 +26,19 @@ class TaskListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val rootView = inflater.inflate(R.layout.fragment_task_list, container, false)
-        val recyclerView = rootView.findViewById<RecyclerView>(R.id.recyclerView)
-        adapter.currentList = taskList
-        recyclerView.adapter = adapter
-        return rootView
+        return inflater.inflate(R.layout.fragment_task_list, container, false)
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        // Vous pouvez également mettre ce code dans onCreateView si vous le préférez
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.adapter = adapter
+
+        val floatingActionButton = view.findViewById<FloatingActionButton>(R.id.fabAddTask)
+        floatingActionButton.setOnClickListener {
+            val newTask = Task(id = UUID.randomUUID().toString(), title = "New task")
+            taskList = taskList + newTask
+            adapter.submitList(taskList)
+        }
     }
 }
