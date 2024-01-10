0cbd04c076b832057b02fb45290e561682d509df
Ariel <ariel.lissak@gmail.com>
2024-01-10 18:32:28 +0100

    TP3 question6

---
+++ app/src/main/java/com/todoarielthibault/todo/user/UserUpdate.kt
@@ -0,0 +1,10 @@
+package com.todoarielthibault.todo.user
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UserUpdate(
+        @SerialName("full_name")
+        val name: String
+)
